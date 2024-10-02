13f39af8acbab37ddfa1840f71fc79f5c5a74eb8
Leandro Barbosa <101986070+LeandroBarbosa753@users.noreply.github.com>
2024-10-02 19:19:53 +0000

    🚀 feat: Criação e atualização das rotas de autenticação e recursos
    Adicionadas as rotas de sessão para login e logout com middleware de autenticação. Criadas rotas para Adms com controle de acesso, e adicionado grupo de rotas para Alunos e Turmas protegido por autenticação.

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,14 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+
+Route.post('/session', 'SessionsController.store')
+Route.delete('/session', 'SessionsController.destroy').middleware('auth')
+
+Route.resource('/adm', 'AdmsController').apiOnly()
+
+Route.group(() => {
+    Route.resource('/aluno', 'AlunosController').apiOnly()
+    Route.resource('/turma', 'TurmasController').apiOnly()
+}).middleware('auth')
+
